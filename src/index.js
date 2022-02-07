078649af5d3f27097f47de2ced0134659f5a2223
Okia02 <anajimenezmelero@gmail.com>
2022-02-07 11:30:24 +0100

    add 4.5exercises

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const movies = require("./data/movies.json");
-const users = require("./data/users.json");
+//const users = require("./data/users.json");
+const Database = require("better-sqlite3");
+const db = new Database(
+  "./src/db/database.db",
+  { verbose: console.log }
+);
 
 // create and config server
 const server = express();
@@ -12,39 +16,70 @@ server.use(express.json());
 server.set("view engine", "ejs");
 
 //Enpoint to 1 movie
-server.get("/movie/:movieId", (req, res) => {
-  const movieId = req.params.movieId;
-  const foundMovie = movies.movies.find((movie) => movie.id === movieId);
-  res.render("movie", foundMovie);
-});
+server.get(
+  "/movie/:movieId",
+  (req, res) => {
+    const movieId = req.params.movieId;
+    const query = db.prepare(
+      "SELECT * FROM movies WHERE id = ?"
+    );
+    const foundMovie =
+      query.get(movieId);
+    res.render("movie", foundMovie);
+  }
+);
 
 //servidor de estáticos
-const staticServerPath = "./public";
-server.use(express.static(staticServerPath));
+const staticServerPath =
+  "./src/public-react";
+server.use(
+  express.static(staticServerPath)
+);
 
-const staticServerPathStyles = "./src/styles";
-server.use(express.static(staticServerPathStyles));
+const staticServerPathStyles =
+  "./src/styles";
+server.use(
+  express.static(staticServerPathStyles)
+);
 
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+  console.log(
+    `Server listening at http://localhost:${serverPort}`
+  );
 });
 
 server.get("/movies", (req, res) => {
   const filterGender = req.query.gender;
-  const cleanedMovies = movies.movies;
-  const filteredMovies = cleanedMovies.filter((movie) => {
-    if (filterGender === "") {
-      return movie;
+  const sort = req.query.sort;
+  if (filterGender === "") {
+    const query = db.prepare(
+      "SELECT * FROM movies"
+    );
+    const allMovies = query.all();
+    return res.json({
+      success: true,
+      movies: allMovies,
+    });
+  } else {
+    const query = db.prepare(
+      "SELECT * FROM movies WHERE gender = ? ORDER BY title DESC"
+    );
+    const foundMovies = query.all(
+      filterGender
+    );
+    if (foundMovies) {
+      return res.json({
+        success: true,
+        movies: foundMovies,
+      });
     } else {
-      return movie.gender === filterGender;
+      return res.json(
+        "No hay coincidencias."
+      );
     }
-  });
-  res.json({
-    success: true,
-    movies: filteredMovies,
-  });
+  }
 });
 
 //POST login
@@ -52,26 +87,40 @@ server.get("/movies", (req, res) => {
 server.post("/login", (req, res) => {
   const email = req.body.email;
   const pass = req.body.password;
-  if (users.find((user) => user.email === email)) {
-    if (users.find((user) => user.password === pass)) {
+  const query = db.prepare(
+    "SELECT * FROM users WHERE email = ? and password = ?"
+  );
+  const foundUser = query.get(
+    email,
+    pass
+  );
+  console.log(foundUser);
+
+  if (foundUser) {
+    if (foundUser.password === pass) {
       res.json({
         success: true,
-        userId: "123",
+        userId: foundUser.id,
       });
     } else {
       res.json({
         success: false,
-        errorMessage: "Contraseña incorrecta",
+        errorMessage:
+          "Contraseña incorrecta",
       });
     }
   } else {
     res.json({
       success: false,
-      errorMessage: "Usuario no encontrado",
+      errorMessage:
+        "Usuario no encontrado",
     });
   }
 });
 
 //servidor de estáticos (imágenes)
-const staticServerPathImg = "./src/public-movies-images";
-server.use(express.static(staticServerPathImg));
+const staticServerPathImg =
+  "./src/public-movies-images";
+server.use(
+  express.static(staticServerPathImg)
+);
